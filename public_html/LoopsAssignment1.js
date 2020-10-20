a32e18b225c065386925f64da061ae76d76fed50
Simon Mollina <Simon Mollina@SimonsPC>
2020-10-19 20:44:55 -0500

    Initial Commit
    Added rest of code to finish

--- public_html/LoopsAssignment1.js
+++ public_html/LoopsAssignment1.js
@@ -2,6 +2,12 @@
  * Display the numbers from 0 to 1000 by 10's
  * 0, 10, 20, 30, ... 1000
  */
-
-document.write("This program displays the numbers 0 to 1000 by 10's");
-
+var i = 10;
+var counter = 1000;
+while (i  <=counter){
+        document.write("<br>" + i); 
+        
+        i = i + 10; 
+    }
+    
+      document.write("This program displays the numbers 0 to 1000 by 10's");  
